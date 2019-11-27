b1b62121b88956a3dc2b40bb9bdc01a1fab7d289
Rahul <rsangole@apple.com>
2019-11-27 00:53:42 -0800

    Adds feature to mark anomalies and plot them too

--- app.R
+++ app.R
@@ -23,31 +23,25 @@ sidebar <- dashboardSidebar(sidebarMenu(
                    "text/comma-separated-values,text/plain",
                    ".csv")
     ),
-    prettyCheckbox(
-        inputId = "chkbox_overlaycategories",
-        label = "Overlay Categories", 
-        value = TRUE
-    ),    
-    prettyCheckbox(
-        inputId = "chkbox_showlegend",
-        label = "Show Legend", 
-        value = FALSE
-    ),
+    uiOutput("cat_list"),
     uiOutput("daterange"),
-    uiOutput("cat_list")
-    # menuItem("Plots", tabName = "tab_plots")
+    actionButton("mark", "Mark Anomaly"),
+    prettyCheckbox(
+        inputId = "chkbox_showanomalies",
+        label = "Show Anomalies",
+        value = TRUE
+    )
 ))
 
-body <- dashboardBody(
-    tabsetPanel(
-        tabPanel("Current category",
-                 plotOutput("tsplot", brush = "user_brush", click = "plot_click"),
-                 h2('Selected points'),
-                 reactableOutput("outtable"),
-                 verbatimTextOutput("info")
-                 )
-        )
-)
+body <- dashboardBody(tabsetPanel(
+    tabPanel(
+        "Current category",
+        plotOutput("tsplot", brush = "user_brush", click = "plot_click"),
+        h2('Selected points'),
+        reactableOutput("outtable"),
+        verbatimTextOutput("info")
+    )
+))
 
 ui <- dashboardPage(
     header = shinydashboardPlus::dashboardHeaderPlus(title = "TS Labeler"),
@@ -57,83 +51,133 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
-    read_data <- reactive({
-        req(input$filein_rawdata)
-        infile <- input$filein_rawdata
-        out <- fread(file = infile$datapath, 
-              col.names = c("ds","cat","value"),
-              key = "ds")
-        out[,ds:=lubridate::fast_strptime(ds, format = "%Y-%m-%d %H:%M:%S",
-                                          tz = "UTC", lt = FALSE)]
-        out[,cat:=as.character(cat)]
-        out[,value:=as.numeric(value)]
-        out
-    })
+    values <- reactiveValues()
     
+    observeEvent(input$filein_rawdata, {
+        infile <- input$filein_rawdata
+        if (is.null(infile))
+            return(NULL)
+        
+        out <- fread(
+            file = infile$datapath,
+            col.names = c("ds", "cat", "value"),
+            key = "ds"
+        )
+        out[, ds := lubridate::fast_strptime(ds,
+                                             format = "%Y-%m-%d %H:%M:%S",
+                                             tz = "UTC",
+                                             lt = FALSE)]
+        out[, cat := as.character(cat)]
+        out[, value := as.numeric(value)]
+        out[, anomaly := 0]
+        out[, tag := NA]
+        
+        values$original <- out
+    })
+
     output$cat_list <- renderUI({
         req(input$filein_rawdata)
-        dat <- read_data()
         pickerInput(
             inputId = "picker_categories",
-            label = "TS Category", 
-            choices = dat[,unique(cat)],
+            label = "TS Category",
+            choices = values$original[, unique(cat)],
             options = list(`live-search` = TRUE),
             multiple = TRUE
         )
     })
     
-    output$cat_list <- renderUI({
+    output$daterange <- renderUI({
         req(input$filein_rawdata)
-        dat <- read_data()
-        dateRangeInput('dateRange',
-                       label = 'Date range',
-                       start = dat[,min(ds)], 
-                       end = dat[,max(ds)] 
+        dateRangeInput(
+            'daterange',
+            label = 'Date range',
+            start = values$original[, min(ds)],
+            end = values$original[, max(ds)]
         )
     })
     
     filtered_data <- reactive({
-        dat <- read_data()
-        dat[cat %in% input$picker_categories]
+        values$original[cat %in% input$picker_categories &
+                            ds >= as.POSIXct(input$daterange[1], tz = "UTC") &
+                            ds <= as.POSIXct(input$daterange[2], tz = "UTC")]
     })
     
     output$tsplot <- renderPlot({
         withProgress({
             dat <- filtered_data()
             
-            cat_filtered <- dat[,unique(cat)]
+            cat_filtered <- dat[, unique(cat)]
             
-            plot(dat[cat == cat_filtered[1],ds],
-                 dat[cat == cat_filtered[1],value],
-                 type="l",
-                 col="red",lty=1,lwd=2, 
-                 ylim=c( min(dat$value, na.rm=T),
-                         max(dat$value, na.rm=T)),
-                 xlab = "Date",
-                 ylab = "Value")
-            for(i in 1:length(cat_filtered)){ 
-                lines(x = dat[cat==cat_filtered[i],ds],
-                      y = dat[cat==cat_filtered[i],value],
-                      type="l",
-                      col=i,
-                      lty=1,lwd=2)
-            } 
+            plot(
+                dat[cat == cat_filtered[1], ds],
+                dat[cat == cat_filtered[1], value],
+                type = "l",
+                col = "red",
+                lty = 1,
+                lwd = 2,
+                ylim = c(min(dat$value, na.rm = T),
+                         max(dat$value, na.rm = T)),
+                xlab = "Date",
+                ylab = "Value"
+            )
+            if (input$chkbox_showanomalies) {
+                subdat <- dat[cat == cat_filtered[1] &
+                                  anomaly == 1]
+                points(subdat[, ds],
+                       subdat[, value],
+                       col = "red",
+                       pch = 19)
+            }
+            for (i in 1:length(cat_filtered)) {
+                lines(
+                    x = dat[cat == cat_filtered[i], ds],
+                    y = dat[cat == cat_filtered[i], value],
+                    type = "l",
+                    col = i,
+                    lty = 1,
+                    lwd = 2
+                )
+                if (input$chkbox_showanomalies) {
+                    subdat <- dat[cat == cat_filtered[i] &
+                                      anomaly == 1]
+                    points(subdat[, ds],
+                           subdat[, value],
+                           col = "red",
+                           pch = 19)
+                }
+            }
+            legend(
+                "topleft",
+                legend = cat_filtered,
+                col = 1:length(cat_filtered),
+                bg = "white",
+                lwd = 2
+            )
+            
         }, message = "Loading graph...")
     })
     
     output$outtable <- renderReactable(reactable(selectedPoints()))
     
-    output$info <- renderText({
-        paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+    selectedPoints <- reactive({
+        brushedPoints(
+            df = filtered_data(),
+            brush = input$user_brush,
+            xvar = "ds",
+            yvar = "value"
+        )
     })
     
-    selectedPoints <- reactive({
-        pts <- brushedPoints(df = filtered_data(),
-                             brush = input$user_brush,
-                             xvar = "ds",
-                             yvar = "value")
-        print(pts)
-        pts
+    observeEvent(input$mark, {
+        # dat <- processed_data()
+        values$new <- values$original
+        seldat <- selectedPoints()
+        for (i in 1:nrow(seldat)) {
+            values$new[ds == seldat[i, ds] &
+                           cat == seldat[i, cat],
+                       anomaly := 1]
+        }
+        values$original <- values$new
     })
 }
 
